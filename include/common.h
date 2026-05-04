05964138df02dc0326131eb4889425f899546a17
Cédric Thomas <thomascedric67@gmail.com>
2026-05-04 11:33:31 +0200

    checkpoint: fix common.h - rename X86_MOV_RAX_IMM/MEM to X86_MOV_RM_R64/IMM, add X86_SYSCALL_BYTE1/BYTE2, add PAGE_ALIGN_MASK, remove unused MAX_IMPORT_ENTRIES

---
+++ include/common.h
@@ -0,0 +1,43 @@
+#ifndef MY_WINE_COMMON_H
+#define MY_WINE_COMMON_H
+
+#include <stdint.h>
+
+// Memory page constants
+#define PAGE_SIZE         4096
+#define PAGE_MASK         (PAGE_SIZE - 1)
+#define PAGE_ALIGN_MASK   (~(uint64_t)PAGE_MASK)
+
+// x86_64 opcode constants
+#define X86_JMP_RIP         0xFF
+#define X86_MOD_RIP         0x25
+#define X86_REX_W           0x48
+#define X86_MOV_R64_RIP     0x8B
+#define X86_MOV_RIP         0x05
+#define X86_MOV_RAX_RAX     0x00
+#define X86_CALL            0xE8
+#define X86_JMP_REL         0xE9
+#define X86_RET             0xC3
+#define X86_NOP             0x90
+#define X86_MOV_ABS         0xB8
+#define X86_MOV_RM_R64      0x89
+#define X86_MOV_RM_IMM       0xC7
+#define X86_SYSCALL_BYTE1    0x0F
+#define X86_SYSCALL_BYTE2    0x05
+
+// CRT BSS offsets
+#define CRT_BSS_INITENV     0x018
+#define CRT_BSS_ARGC        0x028
+#define CRT_BSS_ARGV        0x020
+
+// Windows pseudo-handle values
+#define HANDLE_CURRENT_PROCESS  0xFFFFFFFF
+#define STD_INPUT_HANDLE_VALUE  0x7FFFFFFF
+#define STD_OUTPUT_HANDLE_VALUE 0x7FFFFFFE
+#define STD_ERROR_HANDLE_VALUE  0x7FFFFFFD
+
+// Array size limits
+#define MAX_THUNK_TARGETS   256
+#define MAX_FLAT_IMPORTS    256
+
+#endif // MY_WINE_COMMON_H
